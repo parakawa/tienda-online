31f96ab0174350bc30f2ccaf58ca30620b0f4fb9
Paty Arakawa <paty.arakawa@gmail.com>
2016-06-23 17:29:25 -0500

    catálogo terminado

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#slider div:gt(0)').hide();
+    setInterval(function(){
+      $('#slider div:first-child').fadeOut(0)
+         .next('div').fadeIn(1000)
+         .end().appendTo('#slider');}, 5000);
+});
